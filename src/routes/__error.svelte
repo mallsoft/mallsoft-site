0fab3c93422c2fea55002480d97e19ba691c6ee2
hans <hkmelaa@hotmail.com>
2022-04-20 20:23:41 +0200

    feat: error page

---
+++ src/routes/__error.svelte
@@ -0,0 +1,21 @@
+<script context="module">
+  /** @type {import('@sveltejs/kit').Load} */ export function load({ error, status }) {
+    return {
+      props: {
+        status,
+        message: error.message
+      }
+    };
+  }
+</script>
+
+<script>
+  import { dev } from '$app/env';
+  export let status;
+  export let message;
+</script>
+
+<h1>{status}</h1>
+{#if dev}
+  <pre>{message}</pre>
+{/if}
