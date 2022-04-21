565f4f208d6d40103bc2b5f720939f1831992792
hans <hkmelaa@hotmail.com>
2022-04-21 10:27:58 +0200

    refactor

--- svelte.config.js
+++ svelte.config.js
@@ -1,19 +1,15 @@
 import adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
 
+const dev = process.env.NODE_ENV === 'development';
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   preprocess: preprocess(),
   kit: {
-    prerender: {
-      default: true
-    },
-    adapter: adapter({
-      fallback: null,
-      precompress: true
-    }),
+    adapter: adapter(),
     paths: {
-      base: process.env.NODE_ENV === 'development' ? '' : '/mallsoft-site'
+      base: dev ? '' : '/mallsoft-site'
     }
   }
 };
