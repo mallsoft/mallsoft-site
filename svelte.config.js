ae18f220b57dc4e62bdc107ebae7018c5b5054ae
hans <hkmelaa@hotmail.com>
2022-04-21 14:10:43 +0200

    feat: just netlify...

--- svelte.config.js
+++ svelte.config.js
@@ -1,19 +1,11 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-netlify';
 import preprocess from 'svelte-preprocess';
-
-const dev = process.env.NODE_ENV === 'development';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   preprocess: preprocess(),
   kit: {
-    prerender: {
-      default: true
-    },
-    adapter: adapter(),
-    paths: {
-      base: dev ? '' : '/mallsoft-site'
-    }
+    adapter: adapter()
   }
 };
 
