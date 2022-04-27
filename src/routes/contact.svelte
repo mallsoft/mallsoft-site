0bf3212d7e162fbb559014853cfa9753520d6b16
hans <hkmelaa@hotmail.com>
2022-04-27 22:09:36 +0200

    fix: remove copy function

--- src/routes/contact.svelte
+++ src/routes/contact.svelte
@@ -1,24 +1,20 @@
 <script>
-  import { messages } from '$lib/announcement/messages';
+  // import { messages } from '$lib/announcement/messages';
   import { me } from '$lib/content';
 
-  function copyToClip(value) {
-    navigator.clipboard.writeText(value);
-    messages.add('✂', 'Copy that!', `(${value}) copied to clipboard`, 1300);
-  }
+  // function copyToClip(value) {
+  //   navigator.clipboard.writeText(value);
+  //   messages.add('✂', 'Copy that!', `(${value}) copied to clipboard`, 1300);
+  // }
 </script>
 
 <article class="default">
   <h1>Contact</h1>
   <p>
-    <a href="mailto:{me.email}?subject=Hello {me.name}!">mailto:</a><span
-      on:click={() => copyToClip(me.email)}>{me.email}</span
-    >
+    <a href="mailto:{me.email}?subject=Hello {me.name}!">mailto:</a><span>{me.email}</span>
   </p>
   <p>
-    <a href="https://github.com{me.github}">github:</a><span
-      on:click={() => copyToClip(`https://github.com${me.github}`)}>{me.github}</span
-    >
+    <a href="https://github.com{me.github}">github:</a><span>{me.github}</span>
   </p>
 </article>
 
