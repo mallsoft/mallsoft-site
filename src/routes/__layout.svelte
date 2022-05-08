cec9bf95af13db3f163243c47197f07e5ce9a512
hans <hkmelaa@hotmail.com>
2022-05-08 19:28:03 +0200

    refactor: cleanup layout

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,50 +1,14 @@
 <script lang="ts">
-  import Main from '$lib/layout/Main.svelte';
-  import Header from '$lib/layout/Header.svelte';
   import '../app.css';
 
-  import { me } from '$lib/content';
+  import Meta from '$lib/Meta.svelte';
+  import Main from '$lib/layout/Main.svelte';
+  import Header from '$lib/layout/Header.svelte';
   import Announcer from '$lib/announcement/Announcer.svelte';
   import Achievements from '$lib/achievement/Achievements.svelte';
-  import { browser, dev } from '$app/env';
-
-  if (!dev && browser) {
-    const styleStrings = [`font-size: 2em;`, `font-size: 1.2em;`];
-
-    console.info(
-      `
-%c🧐 Hi there!
-%cThis page exists because I needed a homepage and a place to experiment,
-it's hosted on Netlify and built with svelte/svelteKit.
-
-Repo: ${me.repoUrl}
-Feel free to make an issue if you find any broken bits or have any suggestions.
- `,
-      ...styleStrings
-    );
-  }
 </script>
 
-<svelte:head>
-  <meta property="og:title" content="mallsoft" />
-  <meta property="og:site_name" content="mallsoft.dev" />
-  <meta property="og:url" content="https://mallsoft.dev/" />
-  <meta property="og:description" content="mallsoft • dev - developer and designer" />
-  <meta property="og:type" content="website" />
-  <meta property="og:image" content="/opengraph.jpg" />
-
-  <meta name="twitter:title" content="mallsoft" />
-  <meta name="twitter:description" content="mallsoft • dev - developer and designer" />
-  <meta name="twitter:image" content="/opengraph.jpg" />
-  <meta name="twitter:card" content="summary_large_image" />
-
-  <title>mallsoft</title>
-  <meta name="description" content="mallsoft • dev - developer and designer" />
-  <link rel="icon" href="favicon.png" />
-
-  <meta name="theme-color" content="#6b6b6b" />
-</svelte:head>
-
+<Meta />
 <Header />
 
 <Main>
