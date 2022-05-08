8dd970d23cca4a6ee65044b025c1c99f67c23324
hans <hkmelaa@hotmail.com>
2022-05-08 22:36:05 +0200

    feat: add a a loader

---
+++ src/lib/layout/FullscreenLoader.svelte
@@ -0,0 +1,29 @@
+<script>
+  import { navigating } from '$app/stores';
+</script>
+
+{#if $navigating}
+  <div class="loader" aria-hidden="true" />
+{/if}
+
+<style>
+  .loader {
+    position: fixed;
+    bottom: 0;
+    left: 0;
+    width: 10%;
+    height: 5px;
+    background-color: var(---c-b2);
+    z-index: 9999;
+
+    animation: loader 1s linear infinite;
+
+    opacity: 0.7;
+  }
+
+  @keyframes loader {
+    100% {
+      transform: translateX(100vw) scaleX(0.5);
+    }
+  }
+</style>
