185fd7a4236df380082ffc03c1719e83ab343127
hans <hkmelaa@hotmail.com>
2022-05-15 22:15:17 +0200

    .

--- src/lib/Wscord.svelte
+++ src/lib/Wscord.svelte
@@ -1,23 +1,50 @@
 <script>
-  import { onMount } from 'svelte';
-  import { generateName } from './names';
+  import { page } from '$app/stores';
+  let ws;
 
-  onMount(() => {
-    const name = generateName();
-    const socket = new WebSocket('ws://localhost:8080/ws', 'spagetti');
+  function loadSocket() {
+    const socket = new WebSocket('ws://localhost:8080/ws');
 
     socket.onerror = (event) => {
       console.log('error: ', event);
     };
 
     socket.onopen = () => {
-      socket.send(JSON.stringify({ name }));
-      console.log('connected', name);
+      console.log('connected');
     };
 
     socket.onmessage = (event) => {
-      const data = JSON.parse(event.data);
-      console.log('in: ', data);
+      console.log('in: ', event.data);
     };
-  });
+
+    return socket;
+  }
+
+  function sendUpdate(x, y) {
+    if (!ws) {
+      ws = loadSocket();
+    } else {
+      const fullWidth = document.documentElement.clientWidth;
+      const fullHeight = document.documentElement.clientHeight;
+      const scrollX = window.scrollX;
+      const scrollY = window.scrollY;
+
+      const rx = Math.round(((x + scrollX) / fullWidth + Number.EPSILON) * 1000);
+      const ry = Math.round(((y + scrollY) / fullHeight + Number.EPSILON) * 1000);
+      ws.send(`${rx},${ry}${$page.url.pathname}`);
+    }
+  }
+
+  let waiting = false;
+  function handleMouseMove(event) {
+    if (!waiting) {
+      waiting = true;
+      setTimeout(() => {
+        waiting = false;
+        sendUpdate(event.clientX, event.clientY);
+      }, 300);
+    }
+  }
 </script>
+
+<svelte:window on:mousemove={handleMouseMove} />
