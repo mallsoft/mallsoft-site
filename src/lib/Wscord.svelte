e8d80e9982282a19b4f0e588e94ee61c20a00c15
hans <hkmelaa@hotmail.com>
2022-05-26 23:48:50 +0200

    hm

--- src/lib/Wscord.svelte
+++ src/lib/Wscord.svelte
@@ -1,20 +1,48 @@
 <script>
   import { dev } from '$app/env';
+
   let ws;
-  let cursors = null;
+  let cursors = [];
+
+  class Cursor {
+    constructor(name, x, y) {
+      this.name = name;
+      this.x = x;
+      this.y = y;
+      this.alive = true;
+      this._setTimeout();
+    }
+    update(x, y) {
+      this.alive = true;
+      this.x = x;
+      this.y = y;
+      this._setTimeout();
+    }
+    _setTimeout() {
+      clearTimeout(this.ttk);
+      this.ttk = setTimeout(() => {
+        this.alive = false;
+        cursors = cursors;
+      }, 5000);
+      cursors = cursors;
+    }
+  }
 
   const ratio = 100_000; // cent mille
 
   function loadSocket() {
-    const socket = new WebSocket('ws://localhost:8080/ws');
+    const socket = new WebSocket(
+      dev ? 'ws://localhost:8080/ws' : 'wss://socketboi.herokuapp.com/ws'
+    );
 
     socket.onerror = (event) => {
       console.log('error: ', event);
     };
 
     socket.onopen = () => {
-      console.info('socket opened');
-      cursors = [];
+      if (dev) {
+        console.info('socket opened');
+      }
     };
 
     socket.onmessage = (event) => {
@@ -28,11 +56,22 @@
       }
 
       const messages = event.data.split(/\n|\r/);
-      cursors = messages.reduce((acc, cur) => {
+      const incoming = messages.reduce((acc, cur) => {
         const [name, x, y] = cur.split(',');
         acc.push({ name, x, y });
         return acc;
       }, []);
+
+      incoming.forEach((cursor) => {
+        const { name, x, y } = cursor;
+        const { ax, ay } = toAbsolute(x, y);
+        const old = cursors.find((c) => c.name === name);
+        if (old) {
+          old.update(ax, ay);
+        } else {
+          cursors = [...cursors, new Cursor(name, ax, ay)];
+        }
+      });
     };
 
     return socket;
@@ -57,9 +96,10 @@
   }
 
   function sendUpdate(x, y) {
-    if (!ws) {
+    if (ws === undefined) {
       ws = loadSocket();
-    } else {
+    }
+    if (ws.readyState === ws.OPEN) {
       const { rx, ry } = toRelative(x, y);
       ws.send(`${rx},${ry}`);
     }
@@ -80,13 +120,14 @@
 </script>
 
 <svelte:window on:mousemove={handleMouseMove} />
-{#if cursors}
+{#if cursors.length}
   <ul aria-hidden="true">
-    {#each cursors as { name, x, y }}
-      {@const { ax, ay } = toAbsolute(x, y)}
-      <li style:--rx="{ax}px" style:--ry="{ay}px">
-        {name}
-      </li>
+    {#each cursors as { name, x, y, alive }}
+      {#if alive}
+        <li style:--rx="{x}px" style:--ry="{y}px">
+          {name}
+        </li>
+      {/if}
     {/each}
   </ul>
 {/if}
@@ -111,7 +152,6 @@
     transition: top 0.1s, left 0.1s;
 
     font-size: 0.6em;
-    background-color: rebeccapurple;
 
     transform: translate(0.25em, calc(-100% - 0.25em));
   }
