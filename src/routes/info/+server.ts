b3c692b08f5b2d2f6beae677a0084f8ee2a35e0a
hans <hkmelaa@hotmail.com>
2022-10-03 13:01:45 +0200

    fix: sprinkle nice

---
+++ src/routes/info/+server.ts
@@ -0,0 +1,18 @@
+import { json } from '@sveltejs/kit';
+
+let lastReq = null;
+
+/** @type {import('./$types').RequestHandler} */
+export function GET(event) {
+  const { getClientAddress, request } = event;
+
+  const last = lastReq;
+  lastReq = new Date();
+
+  return json({
+    ip: getClientAddress(),
+    ua: request.headers.get('user-agent'),
+    now: lastReq,
+    last
+  });
+}
