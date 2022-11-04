912b15920295603e2e2dde7c941391019447528d
hans <hkmelaa@hotmail.com>
2022-11-04 13:19:37 +0100

    fix: announce toggling

--- src/lib/components/visuals/suggestions/Toggle.svelte
+++ src/lib/components/visuals/suggestions/Toggle.svelte
@@ -1,11 +1,17 @@
 <script>
   import { achievements, unlocked } from '$lib/components/achievement/achievementStores';
+  import { messages } from '$lib/components/announcement/messages';
   import { scribblesEnabled } from './suggestionStore';
 </script>
 
 {#if $unlocked.length === achievements.length}
   <section>
-    Toggle dev scribbles <input type="checkbox" bind:checked={$scribblesEnabled} />
+    Toggle dev scribbles <input
+      type="checkbox"
+      bind:checked={$scribblesEnabled}
+      on:change={() =>
+        messages.add(null, `${$scribblesEnabled ? 'Enabled' : 'Disabled'} scribbles`, '', 400)}
+    />
   </section>
 {/if}
 
