8ae1be687106c6ac1970590a20b7cffa7e2abed8
hans <hkmelaa@hotmail.com>
2022-11-30 12:14:16 +0100

    fix: remove need for achivement complete

--- src/lib/components/visuals/suggestions/Toggle.svelte
+++ src/lib/components/visuals/suggestions/Toggle.svelte
@@ -1,19 +1,16 @@
 <script>
-  import { achievements, unlocked } from '$lib/components/achievement/achievementStores';
   import { messages } from '$lib/components/announcement/messages';
   import { scribblesEnabled } from './suggestionStore';
 </script>
 
-{#if $unlocked.length === achievements.length}
-  <section>
-    Toggle dev scribbles <input
-      type="checkbox"
-      bind:checked={$scribblesEnabled}
-      on:change={() =>
-        messages.add(null, `${$scribblesEnabled ? 'Enabled' : 'Disabled'} scribbles`, '', 400)}
-    />
-  </section>
-{/if}
+<section>
+  Toggle dev scribbles <input
+    type="checkbox"
+    bind:checked={$scribblesEnabled}
+    on:change={() =>
+      messages.add(null, `${$scribblesEnabled ? 'Enabled' : 'Disabled'} scribbles`, '', 400)}
+  />
+</section>
 
 <style>
   input {
