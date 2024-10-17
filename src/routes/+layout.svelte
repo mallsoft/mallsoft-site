0908a7ff9fba4db5cc9cd9446cf4cd6c75a67eef
hans <hkmelaa@hotmail.com>
2024-10-17 21:02:42 +0200

    fouc revert

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,28 +6,4 @@
 
 <Meta />
 
-<svelte:head>
-  <style>
-    body {
-      opacity: 0;
-      transition: opacity 0.2s;
-    }
-  </style>
-</svelte:head>
-
-<noscript>
-  <style>
-    body {
-      transition: opacity 0.2s 1s;
-      opacity: 1 !important;
-    }
-  </style>
-</noscript>
-
-<svelte:window
-  on:load={() => {
-    document.body.style.opacity = '1';
-  }}
-/>
-
 <slot />
