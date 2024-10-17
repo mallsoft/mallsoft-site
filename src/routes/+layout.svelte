ef8a60d97c84d448782ac3c3de4c478790d393ab
hans <hkmelaa@hotmail.com>
2024-10-17 21:16:12 +0200

    take two

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,8 +2,22 @@
   import '@fontsource/space-grotesk';
   import '../app.css';
   import Meta from '$lib/components/layout/Meta.svelte';
+  import { onMount } from 'svelte';
+
+  onMount(() => {
+    document.body.style.transition = 'opacity 0.2s';
+    document.body.style.opacity = '1';
+  });
 </script>
 
 <Meta />
 
 <slot />
+
+<noscript>
+  <style>
+    body {
+      opacity: 1 !important;
+    }
+  </style>
+</noscript>
